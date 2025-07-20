e79f48b4df3933ec592d152077404ed724b1c35d
Haitham Qamhiyeh <git@qamhiyeh.dev>
2025-07-19 20:23:10 -0500

    Scale point values with difficulty

--- settings.py
+++ settings.py
@@ -22,6 +22,8 @@ class Settings:
 
         # How quickly the game speeds up
         self.speedup_scale = 1.3
+        # How quickly the alien point values increase
+        self.score_scale = 1.5
 
         self.initialize_dynamic_settings()
 
@@ -37,7 +39,12 @@ class Settings:
         self.alien_points = 50
 
     def increase_speed(self):
-        """Increase speed settings."""
+        """Increase speed settings and alien point values."""
         self.ship_speed *= self.speedup_scale
         self.bullet_speed *= self.speedup_scale
         self.alien_speed *= self.speedup_scale
+
+        # pylint: disable=attribute-defined-outside-init
+        # pylint is wrong here
+
+        self.alien_points = int(self.alien_points * self.score_scale)
